7a3bd7b1d32393d99eb32fa43b05261eb1671e07
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-21 20:16:59 -0700

    Updated movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-export const MovieCard= ({ movie, onMovieClick }) => {
+/* export const MovieCard= ({ movie, onMovieClick }) => {
   return (
     <div>
       <div>
@@ -17,4 +18,34 @@ export const MovieCard= ({ movie, onMovieClick }) => {
       <button onClick={onMovieClick}>View movie</button>
     </div>
   );
+};
+ */
+
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }).isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birthYear: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
