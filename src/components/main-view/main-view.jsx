324504e99ff588043abc7dff2b13960aeeca5685
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-30 10:02:17 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -67,38 +67,36 @@ export const MainView = () => {
     return <div>The list is empty!</div>;
   }
   
-    return (
+  return (
+    <div>
+      <form>
+        <label>Username</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+  
+        <label>Password</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+  
+        <button type="button" onClick={handleLogin}>Login</button>
+      </form>
+  
       <div>
-        <form>
-          <label>Username</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-  
-          <label>Password</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-  
-          <button type="button" onClick={handleLogin}>Login</button>
-        </form>
-  
-        <div>
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
-                setSelectedMovie(newSelectedMovie);
-              }}
-            />
-          ))}
-          <button
-            onClick={() => {
-              setUser(null);
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
             }}
-          >
-            Logout
-          </button>
-        </div>
+          />
+        ))}
       </div>
-    );
-  };
   
-  export default MainView;
+      <button
+        onClick={() => {
+          setUser(null);
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
