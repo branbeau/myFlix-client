6b5473a09fa862ce3b5639f73b3b526f52e5a14f
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-11-02 15:27:47 -0700

    Updated files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,61 +10,46 @@ export const MainView = () => {
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+}
 
 export const LoginView = ({ onLoggedIn }) => {
   const [loginUsername, setLoginUsername] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [movies, setMovies] = useState([]); 
+}
+
+useEffect(() => {
+  fetch("https://myflixapp-56b818d4e5ca.herokuapp.com/movies")
+    .then(res => res.json())
+    .then(data => {
+      const moviesFromApi = data.map((movie) => ({
+        id: movie._id,
+        title: movie.Title,
+        description: movie.Description,
+        genre: {
+          name: movie.Genre.Name,
+          description: movie.Genre.Description,
+        }
+      }));
+      setMovies(moviesFromApi);
+    });
+});
   
-  const authenticateUser = (username, password) => {
-    // Your authentication logic goes here
-  };
-
-  const generateToken = (user) => {
-    // Your token generation logic goes here
-  };
-
-  const handleLogin = () => {
-    const user = authenticateUser(username, password);
-    const token = generateToken(user);
-
-    onLoggedIn(user, token);
-  };
-  
-  useEffect(() => {
-    fetch("https://myflixapp-56b818d4e5ca.herokuapp.com/movies")
-      .then(res => res.json())
-      .then(data => {
-        const moviesFromApi = data.map((movie) => ({
-          id: movie._id,
-          title: movie.Title,
-          description: movie.Description,
-          genre: {
-            name: movie.Genre.Name,
-            description: movie.Genre.Description,
-          },
-          director: {
-            name: movie.Director.Name,
-            bio: movie.Director.Bio,
-            birthYear: movie.Director.BirthYear
-          }
-        }));
-        setMovies(moviesFromApi);
-      })
-  },[])
-     
   if (!user) {
     return (
       <>
-        <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }} />
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
         or
         <SignupView />
       </>
     );
   }
-
+  
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -77,40 +62,14 @@ export const LoginView = ({ onLoggedIn }) => {
   
   return (
     <div>
-      {/* Render the MovieView component */}
-      { selectedMovie ? <MovieView movie={selectedMovie} /> : null }
-  
-      {/* Render the SignupView component */}
-      { user === null ? <SignupView /> : null }
-  
-      <form>
-        <label>Username</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-  
-        <label>Password</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-  
-        <button type="button" onClick={handleLogin}>Login</button>
-      </form>
-  
-      <div>
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            movie={movie}
-            onMovieClick={(newSelectedMovie) => {
-              setSelectedMovie(newSelectedMovie);
-            }}
-          />
-        ))}
-      </div>
-  
-      <button
-        onClick={() => {
-          setUser(null);
-        }}
-      >
-        Logout
-      </button>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
     </div>
-  )};
+  );
