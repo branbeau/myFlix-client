456416241e9e00b3cb997110d8f833838b8fa48e
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-11-15 12:53:10 -0800

    Revert "Updated files"
    This reverts commit 38d30d0c2bde11cad584fda7a172738b8cf6c4aa.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { Container, Row, Col } from 'react-bootstrap';
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null)
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (!token) return;
-
-    fetch("https://myflixapp-56b818d4e5ca.herokuapp.com/movies", {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-        .then((response) => response.json())
-        .then((movies) => {
-          setMovies(movies);
-   
-        });
-    }, [token]);
-  
-    fetch("https://myflixapp-56b818d4e5ca.herokuapp.com/movies", {
-      headers: { Authorization: Bearer ${token} },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }, [token]);
-
+    fetch("https://myflixapp-56b818d4e5ca.herokuapp.com/movies")
+      .then(res => res.json())
+      .then(data => {
         const moviesFromApi = data.map((movie) => ({
           id: movie._id,
           title: movie.Title,
@@ -45,41 +28,49 @@ export const MainView = () => {
             birthYear: movie.Director.BirthYear,
           }
         }));
-
         setMovies(moviesFromApi);
       })
-  },[])
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-  if (selectedMovie) {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-          </Col>
-        </Row>
-      </Container>
+if (!user) {
+  return (
+    <>
+    <LoginView
+      onLoggedIn={(user, token) => {
+      setUser(user);
+      setToken(token);
+    }}
+    />
+    or
+    <SignupView />
+    </>
     );
-  }
+}
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
 
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
+
   return (
-    <Container>
-      <Row>
-        {movies.map((movie) => (
-          <Col key={movie.id} sm={6} md={4} lg={3}>
-            <MovieCard
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
-                setSelectedMovie(newSelectedMovie);
-              }}
-            />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
   );
-};
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import { Container, Row, Col } from 'react-bootstrap';
 
 import "./index.scss";
 
 function MyFlixApplication (){
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <MainView />
-        </Col>
-      </Row>
-    </Container>
-  );
-}
+  return <MainView />;
+};
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
